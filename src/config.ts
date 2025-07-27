629f48560a04a591eec81f8762e1cd5cee0afd7d
Linnea Gräf <nea@nea.moe>
2025-07-27 11:08:45 +0200

    build: use --experimental-strip-types instead of ts-node for better errors on load

--- src/config.ts
+++ src/config.ts
@@ -9,6 +9,8 @@ const configT = z.object({
   radioName: z.string()
 });
 export type Config = z.infer<typeof configT>;
+// should this be replaced by just a dynamic load of the json file? maybe. this is cool tho.
+rawconfig satisfies Config;
 export const config: Config = configT.parse(rawconfig);
 
 
