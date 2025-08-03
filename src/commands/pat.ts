ce4e0f2f3b07413fa92b6595fd1c66ba080e515c
sunnie <78964224+sunnniee@users.noreply.github.com>
2025-08-03 20:11:06 +0300

    patpat circle avatar

--- src/commands/pat.ts
+++ src/commands/pat.ts
@@ -21,8 +21,9 @@ export default class PatCommand extends Command {
     async run(interaction: ChatInputCommandInteraction, config: Config) {
         const user = interaction.options.getUser('user', true)
         const speed = interaction.options.getInteger('speed') ?? config.commandDefaults.pat.speed
-        const avatarResponse = await fetch(user.avatarURL({})!)
+        const avatarResponse = await fetch(user.avatarURL()!)
         const avatarBuf = Buffer.from(await avatarResponse.arrayBuffer())
+
         const frames: SharpInput[] = []
         const frame_avatar_positions = [
             [32, 102, 73, 65],
@@ -36,18 +37,27 @@ export default class PatCommand extends Command {
             const patImage = await loadImage(filePath);
             const composite = new Canvas(patImage.width, patImage.height)
             const context = composite.getContext('2d');
+
             const [x, y, w, h] = frame_avatar_positions[pos++]
             const squishedAvatar = await loadImage(await sharp(avatarBuf).resize(w, h, {
                 fit: 'fill'
             }).png().toBuffer())
+
+            context.save()
+            context.beginPath()
+            context.ellipse(x + w / 2, y + h / 2, w / 2, h / 2, 0, 0, Math.PI * 2)
+            context.clip()
             context.drawImage(squishedAvatar, x, y)
+            context.restore()
+
             context.drawImage(patImage, 0, 0)
             frames.push(composite.toBuffer('image/png'))
         }
         const webP = await sharp(
             frames,
             { join: { animated: true } }
-        ).webp({ delay: new Array(pos).fill(200-speed), loop: 0 }).toBuffer();
+        ).webp({ delay: new Array(pos).fill(200 - speed), loop: 0 }).toBuffer();
+
         await interaction.reply({
             content: `Patting ${user.displayName}`,
             files: [
