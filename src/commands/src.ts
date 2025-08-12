c569cf3281436945fcd9b9d56f6bc5732e1c09e4
amy <amy@amy.rip>
2025-08-12 05:58:55 +0330

    i think this'll be great

--- src/commands/src.ts
+++ src/commands/src.ts
@@ -7,28 +7,14 @@ import {
 } from "discord.js";
 import { declareCommand } from "../command.ts";
 import { z } from "zod";
+import {NO_EXTRA_CONFIG} from "../config.ts";
 
 export default declareCommand({
     async run(interaction, config) {
         const repo = interaction.options.getString("repo")
-        const meow = await fetch(config.gitapi! + repo).then(res => res.json());
-        const embed = new EmbedBuilder()
-            .setAuthor({
-                name: meow.name,
-            })
-            .setTitle(meow.description)
-            .setImage(meow.owner.avatar_url);
-        const nya = new ActionRowBuilder<ButtonBuilder>().addComponents(new ButtonBuilder().setURL(meow.html_url).setLabel("link").setStyle(ButtonStyle.Link))
-        await interaction.reply({
-            components: [
-                nya
-            ],
-            embeds: [embed],
-        });
+
     },
-    dependsOn: z.object({
-        gitapi: z.string(),
-    }),
+    dependsOn: NO_EXTRA_CONFIG,
     slashCommand: new SlashCommandBuilder()
         .setName("src")
         .setDescription("get src of shit").setIntegrationTypes([
