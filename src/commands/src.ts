84e6185ced987691162dccd52c06de7b7732b667
sunnie <78964224+sunnniee@users.noreply.github.com>
2025-08-13 12:02:31 +0300

    and this is the gitter

--- src/commands/src.ts
+++ src/commands/src.ts
@@ -9,10 +9,10 @@ import * as tmp from 'tmp'
 import * as git from '@napi-rs/simple-git'
 import { declareCommand } from "../command.ts";
 import { z } from "zod";
-import {NO_EXTRA_CONFIG} from "../config.ts";
-import {rm} from "fs/promises"
+import { NO_EXTRA_CONFIG } from "../config.ts";
+import { rm } from "fs/promises"
 import analyse from "linguist-js";
-import {getTop3Languages} from "../util.ts";
+import { getTop3Languages } from "../util.ts";
 export default declareCommand({
     async run(interaction, config) {
         await interaction.deferReply();
@@ -26,8 +26,26 @@ export default declareCommand({
         const repo = git.Repository.clone(reponame, tmpobj.name)
         const resp = getTop3Languages(await analyse(tmpobj.name))
 
-        await interaction.followUp(`${resp[0].language} is the top language in this repo with ${resp[0].percentage}% code`);
-        await rm(tmpobj.name, {recursive: true})
+        const commits = [...repo.revWalk().setSorting(git.Sort.Time).push(repo.head().target()!)].map(
+            c => repo.findCommit(c)!
+        )
+        const topContributors = Object.entries(commits.reduce((obj, c) => {
+            const name = c.author().name()!
+            if (obj[name]) obj[name]++
+            else obj[name] = 1
+            return obj
+        }, {} as Record<string, number>))
+            .sort((a, b) => b[1] - a[1])
+            .slice(0, 3)
+            .map(([name, count]) => `${name} with ${count} commits`)
+            .join(", ")
+
+        await interaction.followUp(`## <${reponame}>
+${commits.length} commits
+Top contributors: ${topContributors}
+Last commit was <t:${commits[0].time().getTime() / 1000}>
+${resp[0].language} is the top language in this repo with ${resp[0].percentage}% code`);
+        await rm(tmpobj.name, { recursive: true })
     },
     dependsOn: NO_EXTRA_CONFIG,
     slashCommand: new SlashCommandBuilder()
